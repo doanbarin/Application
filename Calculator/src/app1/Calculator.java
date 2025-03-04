0eb48bd1249fd4c906f0c96e36ec44663a5236c6
Doan Ba Rin <157820937+doanbarin@users.noreply.github.com>
2025-03-04 17:03:23 +0700

    Update Calculator.java

--- Calculator/src/app1/Calculator.java
+++ Calculator/src/app1/Calculator.java
@@ -175,7 +175,7 @@ public class Calculator extends javax.swing.JFrame {
     private void rfsBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_rfsBtnActionPerformed
         result=0;
         NumText.setText("");
-    }//GEN-LAST:event_rfsBtnActionPerformed
+    }
 
     private void resultBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_resultBtnActionPerformed
         calculate();
